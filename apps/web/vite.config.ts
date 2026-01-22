fb8b968042adcf47a6c78fbd0969d50f8b1c00bf
kottapally-2001 <saicharan.kottapally@gmail.com>
2026-01-22 17:30:07 +0530

    Fix invalid project.json build target

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -3,8 +3,8 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
+  root: path.resolve(__dirname, '.'),
   plugins: [react()],
-  root: 'apps/web',
   build: {
     outDir: path.resolve(__dirname, '../../dist/apps/web'),
     emptyOutDir: true
